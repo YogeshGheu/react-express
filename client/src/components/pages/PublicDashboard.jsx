f384d9cdff4a3ed56409ae22dfeab134058bf64b
yogeshgheu <yogeshgheu@gmail.com>
2024-07-22 21:27:47 +0530

    add a table on the dashboard where all the users and their item counts will be listed

--- client/src/components/pages/PublicDashboard.jsx
+++ client/src/components/pages/PublicDashboard.jsx
@@ -9,6 +9,10 @@ const PublicDashboard = () => {
     const [emailOrShopName, setEmailOrShopName] = useState("")
     const [products, setProducts] = useState([])
     const [errorMessage, setErrorMessage] = useState('')
+    const [shops, setShops] = useState([
+        { name: "yogesh", itemCount: 20 }
+    ])
+
 
     const handleChange = function (e) {
         setEmailOrShopName(e.target.value)
@@ -36,42 +40,101 @@ const PublicDashboard = () => {
         }
     }
 
+    useEffect(() => {
+        const findShops = async function () {
+            try {
+                const res = await fetch("/api/public-user/get-shops", {
+                    method: "GET",
+                })
+                const response = await res.json()
+                if (!response.success) {
+                    setErrorMessage(response.message)
+                    return setShops([{"name":"error occured", itemCount:"not found"}])
+                }
+                setShops(response.shops)
+            } catch (error) {
+                console.log("Something went wrong - ", error)
+                return setShops([{"name":"error occured", itemCount:"not found"}])
+
+            }
+        }
+        findShops()
+    }, [])
+    
+    
+
+
     return (
         <>
             <Navbar />
-            <div className='flex flex-row flex-wrap max-h-[640px] min-h-[640px] overflow-scroll'>
-                {!showProductContainer ? <div className='flex items-start justify-center min-h-full min-w-full'>
-                    <div className='flex flex-col justify-center items-center'>
-                        <div className='mt-5 flex items-center justify-center gap-3'>
-                            <label htmlFor="usernameOrEmail" className="block text-sm font-medium text-gray-700">
-                                Email/Shop Name
-                            </label>
-                            <input
-                                type="text"
-                                name="usernameOrEmail"
-                                id="usernameOrEmail"
-                                value={emailOrShopName}
-                                onChange={handleChange}
-                                required
-                                className=" px-3 py-2 mt-1 border border-gray-300 rounded-md"
-                            />
+            <div className='flex flex-row flex-wrap max-h-[640px] min-h-[calc(100vh-140px)] overflow-scroll'>
+                {!showProductContainer ?
 
-                            <button
-                                type="submit"
-                                className="px-4 py-2 text-white bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                onClick={findProducts}
-                            >
-                                Find Products
-                            </button>
-                        </div>
-                        {errorMessage && (
-                            <div className="text-red-500 text-sm">
-                                {errorMessage}
+                    <div className=' flex-colflex items-start justify-center min-h-full min-w-full'>
+                        <div className='flex mx-auto flex-col justify-center items-center'>
+                            <div className='mt-5 flex items-center justify-center gap-3'>
+                                <label htmlFor="usernameOrEmail" className="block text-sm font-medium text-gray-700">
+                                    Email/Shop Name
+                                </label>
+                                <input
+                                    type="text"
+                                    name="usernameOrEmail"
+                                    id="usernameOrEmail"
+                                    value={emailOrShopName}
+                                    onChange={handleChange}
+                                    required
+                                    className=" px-3 py-2 mt-1 border border-gray-300 rounded-md"
+                                />
+
+                                <button
+                                    type="submit"
+                                    className="px-4 py-2 text-white bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                    onClick={findProducts}
+                                >
+                                    Find Products
+                                </button>
                             </div>
-                        )}
-                    </div>
+                            {errorMessage && (
+                                <div className="text-red-500 text-sm">
+                                    {errorMessage}
+                                </div>
+                            )}
+                        </div>
 
-                </div> :
+                        <div className="w-[480px] mt-10 mb-20 mx-auto bg-white shadow-md rounded-md overflow-hidden">
+                            <table className="min-w-full bg-white">
+                                <thead>
+                                    <tr>
+                                        <th
+                                            colSpan="3"
+                                            className="py-4 px-6 bg-gray-800 text-white text-center text-lg font-semibold"
+                                        >
+                                            Available Shops
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th className="py-2 px-3 bg-gray-100 text-left text-sm font-semibold text-gray-700">Sr.</th>
+                                        <th className="py-2 bg-gray-100 text-center text-sm font-semibold text-gray-700">Shop Name</th>
+                                        <th className="py-2  bg-gray-100 text-center text-sm font-semibold text-gray-700">Item Count</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                            <div className="max-h-44 overflow-y-auto">
+                                <table className="min-w-full bg-white">
+                                    <tbody>
+                                        {shops.map((shop, index) => (
+                                            <tr key={index} className="border-b">
+                                                <td className="py-2 px-4 text-left text-sm text-gray-700">{index + 1}</td>
+                                                <td className="py-2 text-left text-sm text-gray-700">{shop.name}</td>
+                                                <td className="py-2 text-left px-[10px] text-sm text-gray-700">{shop.itemCount}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    :
                     (
                         products.map((product) => {
                             return (
@@ -108,7 +171,17 @@ const PublicDashboard = () => {
                         })
                     )
                 }
+
+
+
             </div>
+
+
+
+
+
+
+
 
             <Footer />
         </>
